0292e93f17687062f98d0d3976ef387d91bd9c7a
Germán Rodríguez <germanson@gmail.com>
2021-04-22 18:37:41 -0700

    Adding sera por eso single, removing scroll animations and added top logo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
-import Fade from "react-reveal/Fade";
+import Image from "next/image";
 
 import SpotifyFollow from "../components/SpotifyFollow";
 import P from "../components/styled/P";
@@ -16,67 +16,57 @@ const Section = styled.section`
 
 const Home = () => (
   <Container>
-    <Fade bottom>
-      <Section>
-        <h1>SVNT</h1>
-        <P>
-          SVNT (Savant) es una banda indie mexicana que existe en la imaginación
-          de un niño con sindrome Savant.
-        </P>
-        <SpotifyFollow />
+    <Section>
+      <Image src="/symbol.png" alt="SVNT Logo" width={50} height={50} />
+      <h1>SVNT</h1>
+      <P>
+        SVNT (Savant) es una banda indie mexicana que existe en la imaginación
+        de un niño con sindrome Savant.
+      </P>
+      <SpotifyFollow />
 
-        <P>
-          <a href="https://open.spotify.com/artist/0kyOA8rutuOL3DwaveHsnG?si=UxzKAelZTgGsd7ho2Af80w">
-            Spotify
-          </a>
-          ,<a href="https://www.instagram.com/svntmx/">Instagram</a>,
-          <a href="https://www.facebook.com/SVNTMX">Facebook</a>,
-          <a href="https://www.youtube.com/c/SVNTMX">Youtube</a>
-        </P>
-      </Section>
-    </Fade>
+      <P>
+        <a href="https://open.spotify.com/artist/0kyOA8rutuOL3DwaveHsnG?si=UxzKAelZTgGsd7ho2Af80w">
+          Spotify
+        </a>
+        ,<a href="https://www.instagram.com/svntmx/">Instagram</a>,
+        <a href="https://www.facebook.com/SVNTMX">Facebook</a>,
+        <a href="https://www.youtube.com/c/SVNTMX">Youtube</a>
+      </P>
+    </Section>
 
-    <Fade bottom>
-      <Section>
-        <h2>Último lanzamiento</h2>
-        <Video videoId="MYdFA6MF2Qk" />
-      </Section>
-    </Fade>
+    <Section>
+      <h2>Último lanzamiento</h2>
+      <Video videoId="S9RpodZGNT8" />
+    </Section>
 
-    <Fade bottom>
-      <Section>
-        <h2>Videos</h2>
-        <Video videoId="Y-L-_g_rqog" />
-      </Section>
-    </Fade>
+    <Section>
+      <h2>Videos</h2>
+      <Video videoId="Y-L-_g_rqog" />
+    </Section>
 
-    <Fade bottom>
-      <Section>
-        <h2>
-          Energía Oscura <small>Sencillos 2021</small>
-        </h2>
-        <Video square videoId="MYdFA6MF2Qk" /> {/* Energía Oscura */}
-        <Video square videoId="xloitqXKZnI" /> {/* Desfrag */}
-      </Section>
-    </Fade>
+    <Section>
+      <h2>
+        Energía Oscura <small>Sencillos 2021</small>
+      </h2>
+      <Video square videoId="S9RpodZGNT8" /> {/* Será por eso */}
+      <Video square videoId="MYdFA6MF2Qk" /> {/* Energía Oscura */}
+      <Video square videoId="xloitqXKZnI" /> {/* Desfrag */}
+    </Section>
 
-    <Fade bottom>
-      <Section>
-        <h2>
-          Cosmogonía <small>Disco Completo 2017</small>
-        </h2>
-        <Video square videoId="xkvzACdyhy0" />
-      </Section>
-    </Fade>
+    <Section>
+      <h2>
+        Cosmogonía <small>Disco Completo 2017</small>
+      </h2>
+      <Video square videoId="xkvzACdyhy0" />
+    </Section>
 
-    <Fade bottom>
-      <Section>
-        <h2>Contacto</h2>
-        <P>
-          Para contactar a la banda <a href="/contacto">da click aquí.</a>
-        </P>
-      </Section>
-    </Fade>
+    <Section>
+      <h2>Contacto</h2>
+      <P>
+        Para contactar a la banda <a href="/contacto">da click aquí.</a>
+      </P>
+    </Section>
   </Container>
 );
 
